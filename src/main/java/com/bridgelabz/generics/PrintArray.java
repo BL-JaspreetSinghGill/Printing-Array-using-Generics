c9f16f280cd0766cd4b4cf78c2ad5083ba87bd2a
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-02-10 18:42:30 +0530

    ADDED: methods to print Integer, Double and Character array

--- src/main/java/com/bridgelabz/generics/PrintArray.java
+++ src/main/java/com/bridgelabz/generics/PrintArray.java
@@ -2,6 +2,30 @@ package com.bridgelabz.generics;
 
 public class PrintArray {
     public static void main(String[] args) {
-        
+        Integer integerArray[] = {1, 2, 3};
+        Double doubleArray[] = {1.1, 2.2, 3.3};
+        Character characterArray[] = {'a', 'b', 'c'};
+
+        toPrint(integerArray);
+        toPrint(doubleArray);
+        toPrint(characterArray);
+    }
+
+    private static void toPrint(Integer[] array) {
+        for (Integer i: array) {
+            System.out.println(i);
+        }
+    }
+
+    private static void toPrint(Double[] array) {
+        for (Double i: array) {
+            System.out.println(i);
+        }
+    }
+
+    private static void toPrint(Character[] array) {
+        for (Character i: array) {
+            System.out.println(i);
+        }
     }
 }
