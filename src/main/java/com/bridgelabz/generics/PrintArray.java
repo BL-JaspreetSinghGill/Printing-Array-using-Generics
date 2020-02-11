de2886c917fc5c335daeeb0f58dde5731a77b8b2
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-02-11 09:31:16 +0530

    REFACTORED: print array class with multiple generic parameters

--- src/main/java/com/bridgelabz/generics/PrintArray.java
+++ src/main/java/com/bridgelabz/generics/PrintArray.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.generics;
 
-public class PrintArray<E> {
+public class PrintArray<X, Y, Z> {
 
-    E[] myArray;
+    X[] myXArray;
+    Y[] myYArray;
+    Z[] myZArray;
 
-    public PrintArray(E[] myArray) {
-        this.myArray = myArray;
+    public PrintArray(X[] myXArray, Y[] myYArray, Z[] myZArray) {
+        this.myXArray = myXArray;
+        this.myYArray = myYArray;
+        this.myZArray = myZArray;
     }
 
     public static void main(String[] args) {
@@ -13,13 +17,13 @@ public class PrintArray<E> {
         Double doubleArray[] = {1.1, 2.2, 3.3};
         Character characterArray[] = {'a', 'b', 'c'};
 
-        new PrintArray<Integer>(integerArray).toPrint();
-        new PrintArray<Double>(doubleArray).toPrint();
-        new PrintArray<Character>(characterArray).toPrint();
+        new PrintArray<Integer, Double, Character>(integerArray, doubleArray, characterArray).toPrint();
     }
 
     private <T> void toPrint() {
-        toPrint(myArray);
+        toPrint(myXArray);
+        toPrint(myYArray);
+        toPrint(myZArray);
     }
 
     private static <T> void toPrint(T[] array) {
