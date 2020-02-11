ef1bad89e81e732b367d0b545ac1061b366809a4
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-02-11 09:24:46 +0530

    MODIFIED: the printarray class to generic class

--- src/main/java/com/bridgelabz/generics/PrintArray.java
+++ src/main/java/com/bridgelabz/generics/PrintArray.java
@@ -1,14 +1,25 @@
 package com.bridgelabz.generics;
 
-public class PrintArray {
+public class PrintArray<E> {
+
+    E[] myArray;
+
+    public PrintArray(E[] myArray) {
+        this.myArray = myArray;
+    }
+
     public static void main(String[] args) {
         Integer integerArray[] = {1, 2, 3};
         Double doubleArray[] = {1.1, 2.2, 3.3};
         Character characterArray[] = {'a', 'b', 'c'};
 
-        toPrint(integerArray);
-        toPrint(doubleArray);
-        toPrint(characterArray);
+        new PrintArray<Integer>(integerArray).toPrint();
+        new PrintArray<Double>(doubleArray).toPrint();
+        new PrintArray<Character>(characterArray).toPrint();
+    }
+
+    private <T> void toPrint() {
+        toPrint(myArray);
     }
 
     private static <T> void toPrint(T[] array) {
